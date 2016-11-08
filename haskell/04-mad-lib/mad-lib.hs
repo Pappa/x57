7b44269c9f7e4dd1e102c007c179b5ae1fcdf1d0
Robert Hill <dr.pappa@gmail.com>
2016-11-08 20:02:13 +0000

    04 MadLib

--- haskell/04-mad-lib/mad-lib.hs
+++ haskell/04-mad-lib/mad-lib.hs
@@ -1,9 +1,21 @@
-prompts = ["Enter a noun: ", "Enter a verb: ", "Enter a adjective: ", "Enter a adverb: "]
+import Text.Printf
 
-promptUser prompts = do
-    putStrLn (head prompts)
-    line <- getLine
-    promptUser (tail prompts)
+getAnswer :: String -> IO String
+getAnswer question = do
+    putStrLn question
+    answer <- getLine
+    return answer
 
-main :: IO ()
-main = promptUser prompts
+getAnswers :: [String] -> [String] -> IO [String]
+getAnswers [] ys = return ys
+getAnswers (x:xs) ys = do
+    answer <- getAnswer x
+    let answers = ys ++ [answer]
+    getAnswers xs answers
+
+main = do
+    let questions = ["Enter a noun:", "Enter a verb:", "Enter an adjective:", "Enter an adverb:"]
+    let madlib = "Your %s is %s up a %s mountain %s."
+    answers <- getAnswers questions []
+    printf madlib (answers!!0) (answers!!1) (answers!!2) (answers!!3)
+    putStrLn ""
